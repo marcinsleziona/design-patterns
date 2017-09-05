ae2994e415c7950f69bb1e71b63318e18f5e407d
Marcin Sleziona <marcin.sleziona@gmail.com>
2017-09-05 11:43:57 +0200

    add decorator pattern

---
+++ structural-facade/src/main/java/pl/ms/designpatterns/facade/FacadeApplication.java
@@ -0,0 +1,11 @@
+package pl.ms.designpatterns.facade;
+
+/*
+ * Created by Marcin on 2017-08-31 15:10
+ */
+public class FacadeApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
